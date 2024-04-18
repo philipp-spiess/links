f0f7a6a21f7cacee8901dc31a4695b2b407563c5
Philipp Spiess <hello@philippspiess.com>
2024-04-18 22:55:45 +0200

    Various updates

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,48 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request): Promise<Response> {
+		try {
+			const map = await loadLinkMap();
+			const url = new URL(request.url);
+
+			const redirectTo = map.get(url.pathname);
+
+			if (redirectTo) {
+				return new Response("Redirecting to ${redirectTo}", {
+					status: 308,
+					headers: {
+						location: redirectTo,
+						"cache-control": "public, max-age=604800",
+					},
+				});
+			}
+			return new Response(
+				`<h1>Not found 😭</h1><a href="https://spiess.dev">spiess.dev</a>`,
+				{
+					status: 200,
+					headers: {
+						"Content-Type": "text/html; charset=utf-8",
+					},
+				},
+			);
+		} catch (err) {
+			console.error(err);
+			return new Response("Internal server error", { status: 500 });
+		}
 	},
 };
+
+// Loads the links.txt file from GitHub and parses it into a map.
+async function loadLinkMap(): Promise<Map<string, string>> {
+	const links = await fetch(
+		"https://raw.githubusercontent.com/philipp-spiess/links/main/links.txt",
+	).then((r) => r.text());
+	return new Map(
+		links.split("\n").flatMap((l) => {
+			const [path, url] = l.split(" ");
+			if (!path || !url || path.startsWith("#")) {
+				return [];
+			}
+			return [[path, url]];
+		}),
+	);
+}
