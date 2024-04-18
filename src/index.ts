f21d4245e380f092ca388f47288c9a859ca22b6f
Philipp Spiess <hello@philippspiess.com>
2024-04-18 23:06:53 +0200

    Various updates

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
+const LINKS_URL =
+	"https://raw.githubusercontent.com/philipp-spiess/links/main/links.txt";
+
 export default {
 	async fetch(request: Request): Promise<Response> {
 		try {
 			const map = await loadLinkMap();
 			const url = new URL(request.url);
 
-			const redirectTo = map.get(url.pathname);
+			const redirectUrl = map.get(url.pathname);
 
-			if (redirectTo) {
-				return new Response("Redirecting to ${redirectTo}", {
+			if (redirectUrl) {
+				return new Response("Redirecting to ${redirectUrl}", {
 					status: 308,
 					headers: {
-						location: redirectTo,
+						location: redirectUrl,
 						"cache-control": "public, max-age=604800",
 					},
 				});
@@ -33,16 +36,25 @@ export default {
 
 // Loads the links.txt file from GitHub and parses it into a map.
 async function loadLinkMap(): Promise<Map<string, string>> {
-	const links = await fetch(
-		"https://raw.githubusercontent.com/philipp-spiess/links/main/links.txt",
-	).then((r) => r.text());
-	return new Map(
-		links.split("\n").flatMap((l) => {
-			const [path, url] = l.split(" ");
-			if (!path || !url || path.startsWith("#")) {
-				return [];
-			}
-			return [[path, url]];
-		}),
-	);
+	const res = await fetch(LINKS_URL, {
+		cf: {
+			cacheTtl: 60,
+			cacheEverything: true,
+		},
+	});
+	if (!res.ok) {
+		throw new Error(
+			`Failed to fetch ${LINKS_URL}: ${res.status} ${res.statusText}`,
+		);
+	}
+	const links = await res.text();
+
+	const entries: [string, string][] = links.split("\n").flatMap((l) => {
+		const [path, url] = l.split(/\s+/);
+		if (!path || !url || path.startsWith("#")) {
+			return [];
+		}
+		return [[path, url]];
+	});
+	return new Map(entries);
 }
